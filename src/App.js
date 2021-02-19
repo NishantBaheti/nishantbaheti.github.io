f8a94601b9495a7ecdb7763ff65a51f114633431
Nishant Baheti <nishantbaheti.it19@gmail.com>
2021-02-19 22:31:07 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Hey ! This is my portfolio.</h1>
+        <h2> Work in progress !</h2> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
